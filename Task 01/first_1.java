f37e87375427410ccd767b1243d3c7d101291f56
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-05-27 11:36:07 +0530

    Create first_1.java

---
+++ Task 01/first_1.java
@@ -0,0 +1,21 @@
+class PrintDemo{
+	public static void main(String args[]){
+		System.out.println("Welcome to java");
+		System.out.println("My Name is Thivviya")
+		System.out.println('My Father Name is Krishnakumar");
+		System.out.println("My age is 21");
+		
+		System.out.println();
+		
+		System.out.print("My Name is Thivviya");
+		System.out.print("My Father Name is Krishnakumar");
+		System.out.print("My Age is 21");
+		
+		System.out.println();
+		
+		System.out.print"My Name is Thivviya"\n")
+		System.out.print("My Father Name is Krishnakumar\n");
+		System.out.print("My age is 21\n");
+		
+	}
+}
