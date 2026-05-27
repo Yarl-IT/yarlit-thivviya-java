6c5a2d5573e2432722fcae3bf6b224776fcbb61c
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-05-27 14:29:45 +0530

    Create arithmeticoperorts.java

---
+++ Task7/arithmeticoperorts.java
@@ -0,0 +1,15 @@
+class arithmeticoperorts{
+	public static void main (String args[]){
+		int x=10;
+		int y=5;
+		
+		System.out.println("x = " + x );
+		System.out.println("Y = " + y);
+		
+		System.out.println("x + y =" + (x+y));
+		System.out.println("x - y =" + (x-y));
+		System.out.println("x * y =" + (x*y));
+		System.out.println("x / y =" + (x/y));
+		System.out.println("x % y =" + (x%y));
+	}
+}
