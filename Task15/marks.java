82decadcd0442ad41ef7fc2ed8526672c0ab4dcf
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-05-27 14:43:07 +0530

    Create marks.java

---
+++ Task15/marks.java
@@ -0,0 +1,11 @@
+class marks{
+	public static void main(String arg[]){
+		int marks = 101 ;
+		if (marks > 75){
+			System.out.println("pass.");
+		}else{
+			System.out.println("fail.");
+		}
+	}
+}
+ 
