cf197a6df45b67d37cd5dcbad0883b08a15e56cb
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-05-29 11:15:45 +0530

    Create marks_ex1.java

---
+++ Task16/marks_ex1.java
@@ -0,0 +1,25 @@
+class marks_ex1{
+	public static void main(String args[]){
+		int marks = 160;
+		if(marks>100){
+			System.out.println("invaild");
+		}
+		else if (marks>75){
+			System.out.print("A");
+		}
+		else if(marks>65){
+			System.out.println("B");
+		}
+		else if(marks>55){
+			System.out.println("C");
+		}
+		else if(marks>35){
+			System.out.println("D");
+		}
+		else{
+			System.out.print("fail");
+		}
+	}
+		
+}
+	
