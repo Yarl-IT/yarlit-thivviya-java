7bbe743bba4ae5c3c4153218a912cb73f0be5088
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-05-29 12:01:32 +0530

    Create Nestedforloop.java

---
+++ Task24/Nestedforloop.java
@@ -0,0 +1,10 @@
+class Nestedforloop{
+	public static void main(String args[]){
+		for(int y=1;y<=5;y++){
+		for(int x=1;x<=5;x++){
+			System.out.print(x);
+		}	
+		System.out.println(y);
+		}	
+	}
+}
