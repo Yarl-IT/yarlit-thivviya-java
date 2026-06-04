8524fac495401c6cc8717657304a0956eea5a9ab
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-06-04 11:25:01 +0530

    Create Arraywhileloop.java

---
+++ Task30/Arraywhileloop.java
@@ -0,0 +1,10 @@
+class Arraywhileloop{
+	public static void main(String args[]){
+		int [] y={10,20,30,40,50,60};
+		int t=0;
+		while(t<y.length){
+			System.out.println(y[t]);
+			t++;
+		}
+	}
+}
