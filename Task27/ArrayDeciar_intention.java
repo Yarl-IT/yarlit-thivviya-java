7bf0a822c944a85e9f59e057ad44da1858724165
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-06-04 11:28:19 +0530

    Create ArrayDeciar_intention.java

---
+++ Task27/ArrayDeciar_intention.java
@@ -0,0 +1,11 @@
+class ArrayDeciar_intention{
+	public static void main(String args[]){
+		int []y;
+		
+		int []y ={10,20,30,40,50,60};
+		for(int t=0;t<y.length;t++){
+			System.out.println(y[t]);
+		}
+		
+	}
+}
